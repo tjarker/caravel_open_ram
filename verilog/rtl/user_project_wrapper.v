d0a7f2c4b4a5733b061dd117eaa8a407af52db62
Tjark Petersen <tjark-petersen@gmx.de>
2025-11-08 09:09:58 +0100

    add openram instance in user_project_wrapper

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,41 +82,23 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
-`ifdef USE_POWER_PINS
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
-`endif
-
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
-
-    // MGMT SoC Wishbone Slave
-
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in ({io_in[37:30],io_in[7:0]}),
-    .io_out({io_out[37:30],io_out[7:0]}),
-    .io_oeb({io_oeb[37:30],io_oeb[7:0]}),
-
-    // IRQ
-    .irq(user_irq)
-);
+sky130_sram_1kbyte_1rw1r_32x256_8 mem (
+    `ifdef USE_POWER_PINS
+        .vccd1(vccd1),
+        .vssd1(vssd1),
+    `endif
+        .clk0(wb_clk_i),
+        .csb0(wbs_cyc_i),
+        .web0(wbs_we_i),
+        .wmask0(wbs_sel_i),
+        .addr0(wbs_adr_i[7:0]),
+        .din0(wbs_dat_i),
+        .dout0(wbs_dat_o),
+        .clk1(1'b0),
+        .csb1(1'b1),
+        .addr1(8'b0),
+        .dout1()
+    );
 
 endmodule	// user_project_wrapper
 
